0216e3889a97d8edc367836a3d0c01aca553b65e
Csaba Palfi <csaba@palfi.me>
2019-08-28 14:23:12 +0100

    initial commit

---
+++ verify.js
@@ -0,0 +1,33 @@
+const https = require('https')
+
+const secret = '6Le7VbUUAAAAADm3L-7Jnbau2h3DGsyuFneOKPZn'
+
+
+module.exports = (request, response) => {
+
+  const token = request.body.token
+
+  const data = new URLSearchParams({ response: token, secret}).toString()
+
+  const options = {
+    method: 'POST',
+    hostname: 'www.google.com',
+    path: '/recaptcha/api/siteverify',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': data.length
+    }
+  }
+  
+  const req = https.request(options, res => res.pipe(response))
+  
+  req.on('error', () => {
+    response.status(500)
+    response.send({success: false, 'error-codes': ['internal-server-error']})
+  })
+  
+  req.write(data)
+
+  req.end()
+}
+
